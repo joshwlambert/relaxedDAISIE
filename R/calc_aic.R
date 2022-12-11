8717010d6ed0ec9f0d41b7509de0cc4215455704
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-12-11 11:03:44 +0100

    added calc_aic and calc_aicc functions

---
+++ R/calc_aic.R
@@ -0,0 +1,35 @@
+#' Calculates Akaike's information criterion for a maximum likelihood output
+#' of the DAISIE model
+#'
+#' @param results The maximum likelihood parameter estimates and associated log
+#' likelihood and free parameters (degrees of freedom)
+#' @param daisie_data The data the DAISIE model was fit to
+#'
+#' @return A numeric
+#' @export
+#'
+#' @examples
+#' stub
+calc_aic <- function(results, daisie_data) {
+  k <- results$df
+  aic <- -2 * results$loglik + 2 * k
+  aic
+}
+
+#' Calculates the bias-corrected (second-order) Akaike's information criterion
+#' for a maximum likelihood output of the DAISIE model
+#'
+#' @inheritParams calc_aic
+#'
+#' @return A numeric
+#' @export
+#'
+#' @examples
+#' stub
+calc_aicc <- function(results, daisie_data) {
+  k <- results$df
+  aic <- -2 * results$loglik + 2 * k
+  m <- daisie_data[[1]]$not_present + length(daisie_data) - 1
+  aicc <- aic + 2 * k * (k + 1) / m - k - 1
+  aicc
+}
