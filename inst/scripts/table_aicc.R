c604703f8e72025d34487899640e609cc8b67562
Joshua Lambert <j.w.l.lambert@rug.nl>
2022-12-19 17:36:23 +0100

    renamed calc_aicc script to table_aicc and added xtable

--- inst/scripts/table_aicc.R
+++ inst/scripts/table_aicc.R
@@ -51,16 +51,57 @@ names(tab_list) <- islands
 # calculate AICc for each model for each island
 for (i in seq_along(tab_list)) {
   tab_list[[i]] <- cbind(tab_list[[i]], aic = NaN, aicc = NaN)
-  for (j in seq_len(nrow(tab_list[[1]]))) {
+  for (j in seq_len(nrow(tab_list[[i]]))) {
     tab_list[[i]][j, "aic"] <- calc_aic(
       results = tab_list[[i]][j, ],
-      daisie_data = parse(names(tab_list[i]))
+      daisie_data = eval(parse(text = names(tab_list[i])))
     )
     tab_list[[i]][j, "aicc"] <- calc_aicc(
       results = tab_list[[i]][j, ],
-      daisie_data = parse(names(tab_list[i]))
+      daisie_data = eval(parse(text = names(tab_list[i])))
     )
   }
+}
+
+tab_list <- lapply(tab_list, \(x) {
+  x$model <- toupper(x$model)
+  x$model <- gsub(pattern = "CR", replacement = "HR", x = x$model)
+  x$model <- gsub(pattern = "_", replacement = " ", x = x$model)
+  x$model <- gsub(pattern = "LAC", replacement = "$\\\\lambda^c$", x = x$model)
+  x$model <- gsub(pattern = "MU", replacement = "$\\\\mu$", x = x$model)
+  x$model <- gsub(pattern = "LAA", replacement = "$\\\\lambda^a$", x = x$model)
+  x$model <- gsub(pattern = "RR K", replacement = "RR $K'$", x = x$model)
+  x$model <- gsub(pattern = "RR ", replacement = "RR", x = x$model)
+  x
+})
+
+for (i in seq_along(islands)) {
+  tab_list <- lapply(tab_list, \(x) {
+    colnames(x) <- c("Model", "loglik", "DF", "BIC", "AIC", "AICc")
+    x
+  })
+
+  print(
+    xtable::xtable(
+      tab_list[[i]],
+      align = "ccccccc",
+      caption = paste0(
+        "Comparison of Akaike and Bayesian information criterion for the ",
+        islands[i], ". Each model is compared by the maximised ",
+        "loglikelihood, Bayesian information criterion (BIC), Akaike ",
+        "information criterion (AIC) and bias-corrected Akaike information ",
+        "criterion (AICc). The number of free parameters (degrees of freedom) ",
+        "is given for each model (DF)."
+      ),
+      label = paste0("tab:", islands[i], "_aic")
+    ),
+    size = "small",
+    NA.string = "NA",
+    include.rownames = FALSE,
+    hline.after = c(0, 0),
+    sanitize.colnames.function = identity,
+    sanitize.text.function = force
+  )
 }
 
 
