27c82a50b9980c66b21be5a3e6d697cd82b5cc65
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-03-05 19:50:26 +0000

    updated plot_bic_weights_post_hoc script

--- inst/scripts/plot_bic_weights_post_hoc.R
+++ inst/scripts/plot_bic_weights_post_hoc.R
@@ -151,10 +151,23 @@ ggplot2::ggplot(data = bic_tbl) +
     name = "Archipelago",
     labels = c(
       "galapagos" = "Galápagos",
-      "hawaii" = "Hawai'i",
+      "hawaii" = "Hawaii",
       "marquesas" = "Marquesas"
     )
   )
+
+# add rows to create legend subheading
+bic_tbl <- rbind(
+  bic_tbl,
+  data.frame(
+    island = unique(bic_tbl$island),
+    model = c(rep("Relaxed-rate", 3), rep("Two-type", 3)),
+    bic = 0,
+    delta_bic = 0,
+    rel_lik = 0,
+    bic_weight = 1e-10
+  )
+)
 
 
 bic_weights <- ggplot2::ggplot(data = bic_tbl) +
@@ -169,37 +182,42 @@ bic_weights <- ggplot2::ggplot(data = bic_tbl) +
   ggplot2::scale_fill_manual(
     name = "Model",
     breaks = c(
-      "cr_dd", "rr_k", "rr_laa_dd", "rr_lac_dd", "rr_mu_dd", "cr_dd_2type_lac",
-      "cr_dd_2type_mu", "cr_dd_2type_k", "cr_dd_2type_lac_mu",
-      "cr_dd_2type_lac_k", "cr_dd_2type_mu_k", "cr_dd_2type_lac_mu_k"
+      "cr_dd", "Relaxed-rate", "rr_k", "rr_laa_dd", "rr_lac_dd", "rr_mu_dd",
+      "Two-type", "cr_dd_2type_lac", "cr_dd_2type_mu", "cr_dd_2type_k",
+      "cr_dd_2type_lac_mu", "cr_dd_2type_lac_k", "cr_dd_2type_mu_k",
+      "cr_dd_2type_lac_mu_k"
     ),
     labels = c(
-      "HR",
+      "Homogeneous-rate",
+      "**Relaxed-rate**",
       "Carrying Capacity",
       "Anagenesis",
       "Cladogenesis",
       "Extinction",
+      "**Two-type**",
       "HR 2type Cladogenesis",
       "HR 2type Extinction",
-      "HR 2type Carrying_Capacity",
+      "HR 2type Carrying Capacity",
       "HR 2type Cladogenesis Extinction",
       "HR 2type Cladogenesis Carrying Capacity",
       "HR 2type Extinction Carrying Capacity",
       "HR 2type Cladogenesis Extinction Carrying Capacity"
     ),
-    values = c("#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072", "#80B1D3", "#FDB462",
-               "#B3DE69", "#e3e339", "#439c53", "#BC80BD", "#2f2ff7", "#f52f46")
+    values = c("#8DD3C7", "white", "#BEBADA", "#FB8072", "#80B1D3", "#FDB462",
+               "white", "#3876a2", "#df7702", "#655ca7", "#90b500", "#8F00FF",
+               "#C12267", "#006400")
   ) +
   ggplot2::scale_y_continuous(name = "BIC Weight") +
   ggplot2::scale_x_discrete(
     name = "Archipelago",
     labels = c(
       "galapagos" = "Galápagos",
-      "hawaii" = "Hawai'i",
+      "hawaii" = "Hawaii",
       "marquesas" = "Marquesas"
     )
   ) +
-  ggplot2::theme_classic()
+  ggplot2::theme_classic() +
+  ggplot2::theme(legend.text = ggtext::element_markdown())
 
 ggplot2::ggsave(
   plot = bic_weights,
