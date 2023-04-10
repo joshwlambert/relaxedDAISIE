16c13ffc23aacb2c0d2df49e5c8a0f4db99f30e6
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-04-10 17:53:07 +0100

    updated table_aic script

--- inst/scripts/table_aicc.R
+++ inst/scripts/table_aicc.R
@@ -11,7 +11,7 @@ data("SaoTome_Principe")
 
 tab_list <- list()
 models <- c("cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_k", "rr_laa_dd")
-islands <- c("Comoros", "Galapagos", "Marquesas",
+islands <- c("Canaries", "Comoros", "Galapagos", "Hawaii", "Marquesas",
              "New_Caledonia", "SaoTome_Principe")
 
 for (i in seq_along(islands)) {
@@ -25,27 +25,7 @@ for (i in seq_along(islands)) {
   tab_list[[i]] <- tab
 }
 
-canaries_models <- c("cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_laa_dd")
-tab <- data.frame()
-for (model in canaries_models) {
-  best_model <- choose_best_model(data_name = "Canaries", model = model)
-  best_model <- best_model[, c("loglik", "df", "bic")]
-  best_model <- cbind(model = model, best_model)
-  tab <- rbind(tab, best_model)
-}
-tab_list[[length(tab_list) + 1]] <- tab
 
-hawaii_models <- c("cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_k")
-tab <- data.frame()
-for (model in hawaii_models) {
-  best_model <- choose_best_model(data_name = "Hawaii", model = model)
-  best_model <- best_model[, c("loglik", "df", "bic")]
-  best_model <- cbind(model = model, best_model)
-  tab <- rbind(tab, best_model)
-}
-tab_list[[length(tab_list) + 1]] <- tab
-
-islands <- c(islands, "Canaries", "Hawaii")
 names(tab_list) <- islands
 
 # calculate AICc for each model for each island
@@ -72,6 +52,11 @@ tab_list <- lapply(tab_list, \(x) {
   x$model <- gsub(pattern = "LAA", replacement = "$\\\\lambda^a$", x = x$model)
   x$model <- gsub(pattern = "RR K", replacement = "RR $K'$", x = x$model)
   x$model <- gsub(pattern = "RR ", replacement = "RR", x = x$model)
+  x
+})
+
+tab_list <- lapply(tab_list, \(x) {
+  x$Model <- gsub(pattern = " DD", replacement = "", x = x$Model)
   x
 })
 
