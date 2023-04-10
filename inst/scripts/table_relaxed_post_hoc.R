578f7c47d1368d1d52fa52c2e7a48b8a5c64ff5d
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-04-10 17:53:34 +0100

    updated table_relaxed_post_hoc script

--- inst/scripts/table_relaxed_post_hoc.R
+++ inst/scripts/table_relaxed_post_hoc.R
@@ -2,7 +2,7 @@
 
 tab_list <- list()
 models <- c("cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_k", "rr_laa_dd")
-islands <- c("Galapagos", "Marquesas")
+islands <- c("Galapagos", "Hawaii", "Marquesas")
 
 for (i in seq_along(islands)) {
   tab <- data.frame()
@@ -36,44 +36,12 @@ for (i in seq_along(islands)) {
   tab_list[[i]] <- tab
 }
 
-hawaii_models <- c("cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_k")
-tab <- data.frame()
-for (model in hawaii_models) {
-  best_model <- choose_best_model(data_name = "Hawaii", model = model)
-  if (model == "cr_dd") {
-    best_model <- cbind(
-      best_model, sd = NA_real_, lambda_c2 = NA_real_, mu2 = NA_real_,
-      K2 = NA_real_
-    )
-    best_model <-
-      best_model[, c(
-        "lambda_c", "mu", "K", "gamma", "lambda_a", "lambda_c2", "mu2", "K2",
-        "sd", "loglik", "bic"
-      )]
-  } else if (grepl("rr_", model)) {
-    rm_index <- which(colnames(best_model) %in% c("df", "conv"))
-    best_model <- best_model[, -rm_index]
-    best_model <- cbind(
-      best_model, lambda_c2 = NA_real_, mu2 = NA_real_, K2 = NA_real_
-    )
-    best_model <-
-      best_model[, c(
-        "lambda_c", "mu", "K", "gamma", "lambda_a", "lambda_c2", "mu2", "K2",
-        "sd", "loglik", "bic"
-      )]
-  }
-  best_model <- cbind(model = model, best_model)
-  tab <- rbind(tab, best_model)
-}
-tab_list[[length(tab_list) + 1]] <- tab
-
-islands <- c(islands, "Hawaii")
 names(tab_list) <- islands
 
 models <- c("cr_dd_2type_lac", "cr_dd_2type_mu", "cr_dd_2type_k",
             "cr_dd_2type_lac_mu", "cr_dd_2type_lac_k", "cr_dd_2type_mu_k",
             "cr_dd_2type_lac_mu_k")
-islands <- c("Galapagos_2type", "Marquesas_2type", "Hawaii_2type")
+islands <- c("Galapagos_2type", "Hawaii_2type", "Marquesas_2type")
 
 for (i in seq_along(islands)) {
   tab <- tab_list[[i]]
@@ -107,6 +75,12 @@ tab_list <- lapply(tab_list, \(x) {
   colnames(x) <- c("Model", "$\\lambda^c$", "$\\mu$", "$K'$", "$\\gamma$",
                    "$\\lambda^a$", "$\\lambda^c_2$", "$\\mu_2$", "$K'_2$",
                    "$\\sigma$", "loglik", "BIC")
+  x
+})
+
+# remove DD from model names
+tab_list <- lapply(tab_list, \(x) {
+  x$Model <- gsub(pattern = " DD", replacement = "", x = x$Model)
   x
 })
 
