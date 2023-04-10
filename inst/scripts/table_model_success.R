23423291650492d95376dd5414a6a51bf8d031b6
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-04-10 17:53:20 +0100

    updated table_model_success script

--- inst/scripts/table_model_success.R
+++ inst/scripts/table_model_success.R
@@ -6,7 +6,8 @@ model <- c(
   "cr_dd", "rr_lac_dd", "rr_mu_dd", "rr_k", "rr_gam_dd", "rr_laa_dd"
 )
 
-data_name <- c("Canaries", "Comoros", "Galapagos", "Hawaii", "Marquesas", "New_Caledonia", "SaoTome_Principe")
+data_name <- c("Canaries", "Comoros", "Galapagos", "Hawaii", "Marquesas",
+               "New_Caledonia", "SaoTome_Principe")
 
 for (i in seq_along(data_name)) {
 
@@ -40,30 +41,46 @@ model_success$Model <- gsub(pattern = "LAC", replacement = "$\\\\lambda^c$", x =
 model_success$Model <- gsub(pattern = "MU", replacement = "$\\\\mu$", x = model_success$Model)
 model_success$Model <- gsub(pattern = "LAA", replacement = "$\\\\lambda^a$", x = model_success$Model)
 model_success$Model <- gsub(pattern = "RR K", replacement = "RR $K'$", x = model_success$Model)
+model_success$Model <- gsub(pattern = "GAM", replacement = "$\\\\gamma$", x = model_success$Model)
 model_success$Model <- gsub(pattern = "RR ", replacement = "RR", x = model_success$Model)
 
 model_success$Success <- paste0(model_success$Success, "/10")
 
+model_success$Model <- gsub(
+  pattern = " DD",
+  replacement = "",
+  x = model_success$Model
+)
+
+
+for (i in seq_len(nrow(model_success))) {
+  if (i %in% seq(from = 1, to = nrow(model_success), by = 6)) {
+    model_success$Archipelago[i] <- paste0(
+      "\\multirow{", 6, "}{*}{", model_success$Archipelago[i], "}"
+    )
+  } else {
+    model_success$Archipelago[i] <- ""
+  }
+}
+
+
 print(
   xtable::xtable(
     model_success,
-    display = c("s", "s", rep("g", 9)),
-    align = "ccc",
+    display = c("s", "s", "s", "s"),
+    align = "cccc",
     caption = paste0(
-      "Maximum likelihood results for the ", islands[i], " archipelago for ",
-      "a selection of homogeneous-rate (HR) and relaxed-rate (RR) models. ",
-      "Parameters estimated are: cladogenesis ($\\lambda^c$), extinction ",
-      "($\\mu$), carrying capacity (\\textit{K}, colonisation ($\\gamma$), ",
-      "anagenesis ($\\lambda^a$), standard deviation of relaxed parameter ",
-      "($\\sigma$), as well as the models maximised log likelihood (loglik) ",
-      "and Bayesian Information Criterion (BIC)."),
-    label = paste0("tab:", islands[i], "_ml")
+      "Success rate of optimisation of the models used in this study. Models ",
+      "failed most often because the numerical integration of the likelihood ",
+      "became unstable and crashed."
+    ),
+    label = "tab:success_rate"
   ),
   size = "small",
   math.style.exponents = TRUE,
   NA.string = "NA",
   include.rownames = FALSE,
-  hline.after = c(0, 0),
+  hline.after = sort(rep(seq(from = 0, to = nrow(model_success), by = 6), 2)),
   sanitize.colnames.function = identity,
   sanitize.text.function = force
 )
